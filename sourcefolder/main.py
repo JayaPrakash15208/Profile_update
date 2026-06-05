24237ffd58feabed27482d3d3964121670e788db
Pitta Jaya Prakash <163733222+JayaPrakash15208@users.noreply.github.com>
2026-06-05 19:42:23 +0530

    user input removed

--- sourcefolder/main.py
+++ sourcefolder/main.py
@@ -1,26 +1,12 @@
 import naukri 
 import Naukari_resume_update
 
-def profiles():
-    print(f'Which profile fo you want to run:\n')
-    print(f'1.ChandraSekhar\n2.Jay Prakash\n')
 
 
-def runProfile(confirm):
-    while confirm == 'y':
-        profiles()
-        profile = int(input(f'Select Profile:\t'))
-        if profile == 1:
-            naukri.main()
-        elif profile == 2:
-            Naukari_resume_update.main()
-        else:
-            pass
-        confirm=input('Do you want select another profile? y/n:\t')
-        runProfile(confirm)
-    else:
-        print(f'You wish to discontinue')
+def runProfile():
+    naukri.main()
+    print("Naukri profile updated successfully.")
+    Naukari_resume_update.main()
+    print("Naukri profile updated successfully.")
 
-
-confirm = input('Continue to profiles y/n:\t')
-runProfile(confirm)
+runProfile()
